e9e2276947137345582fb35e6b76601d8c20c279
DougMacdonald-Security <macdonald.douglas@gmail.com>
2025-06-27 22:40:37 +0100

    Update audit_report.py

--- mTLS/CA_check/audit_report.py
+++ mTLS/CA_check/audit_report.py
@@ -1,10 +1,10 @@
 aws acm-pca create-certificate-authority-audit-report \
-  --certificate-authority-arn arn:aws:acm-pca:eu-west-1:533267216364:certificate-authority/1ac1407c-41ce-4a24-97d1-ff86184858b1 \
+  --certificate-authority-arn arn:aws:acm-pca:eu-west-1:1234:certificate-authority/1ac14-858b1 \
   --s3-bucket-name gib-pca-dr-crl \
   --audit-report-response-format JSON \
   --region eu-west-1
 
 
 
-arn:aws:acm-pca:eu-west-1:533267216364:certificate-authority/1ac1407c-41ce-4a24-97d1-ff86184858b1 
+arn:aws:acm-pca:eu-west-1:1234:certificate-authority/1ac14-58b1 
 acm-pca:CreateCertificateAuthorityAuditReport
